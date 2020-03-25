0a1d6ea18bc03500f7a49f17118f525efebe7878
Cameron Henlin <cam.henlin@gmail.com>
2020-03-24 22:03:44 -0700

    big upgrade! switch from automator commands back to applescript. requires some additional permissions for grabbing the chat GUID from the message SQLite db if it is available.

--- test.js
+++ test.js
@@ -2,9 +2,13 @@
 var imessage = require("./index.js");
 
 imessage.sendMessage("", "test 1", function() {
-	console.log("should be fine.");
+	console.log("should be fine 1");
 });
 
-imessage.sendMessage("paul come back 🤷‍♀️", "imessage module 2 unit test - can send message with emojis🤷‍♀️\nand new lines", function() {
-	console.log("should be fine.");
+imessage.sendMessage("to group chat", "imessage module unit test - can send message with emojis🤷‍♀️\nand new lines", function() {
+	console.log("should be fine 2");
+});
+
+imessage.sendMessage("direct to an email", "test 3", function() {
+	console.log("should be fine 3");
 });
