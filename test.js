5f0e55ac49cbb37fa5967c1c43ed6460db4c48fa
Cameron Henlin <camh@Camerons-Mac-mini.local>
2021-09-18 00:12:37 -0700

    update back to using automator, working on big sur

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 
 var imessage = require("./index.js");
 
-imessage.sendMessage("", "test 1", function() {
-	console.log("should be fine.");
-});
-
-imessage.sendMessage("paul come back 🤷‍♀️", "imessage module 2 unit test - can send message with emojis🤷‍♀️\nand new lines", function() {
+imessage.sendMessage("Let’s slop ‘em up", "test", function() {
 	console.log("should be fine.");
 });
